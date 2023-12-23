9e644bd94a957a66354eb105c417743fe2d8b5e1
Byron Burks <byron.burks@gmail.com>
2023-12-23 12:57:22 -0600

    implement game state validation

---
+++ AdventTest/BagOfCubesGameTest.cs
@@ -0,0 +1,19 @@
+﻿using BagOfCubesGame;
+using FluentAssertions;
+
+namespace AdventTest {
+    public class BagOfCubesGameTest {
+        public BagState TestBag { get; } = new(12, 13, 14);
+
+        [Theory]
+        [InlineData("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", true)]
+        [InlineData("1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", true)]
+        [InlineData("8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", false)]
+        [InlineData("1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", false)]
+        [InlineData("6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", true)]
+        public void IsValid_GivenTestBag_ShouldCorrectlyComputeGameValidity(string input, bool expected) {
+            var game = new BagOfCubesGameState(input, TestBag);
+            game.IsValid().Should().Be(expected);
+        }
+    }
+}
---
+++ BagOfCubesGame/BagOfCubesGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BagOfCubesGame; 
+
+public static class BagOfCubesGame {
+    public static bool IsValid(this BagOfCubesGameState @this)
+        => @this.TurnsList
+                .Split(';')
+                .Select(turn => new BagOfCubesGameState(turn.Trim(), @this.Bag))
+                .Select(game => game.TurnsList.IsValid(game.Bag))
+                .Aggregate((a, b) => a && b);
+
+    private static bool IsValid(this string @this, BagState bag) {
+        if(@this.Contains(';')) {
+            throw new ArgumentException("string must contain a single turn"); }
+
+        var redCount = GetMatchingCount("red");
+        var greenCount = GetMatchingCount("green");
+        var blueCount = GetMatchingCount("blue");
+
+        return redCount <= bag.RedCount
+            && greenCount <= bag.GreenCount
+            && blueCount <= bag.BlueCount;
+
+        int GetMatchingCount(string color) {
+            var colorPattern = $"(?'number'[0123456789]+)\\s+(?:{color})";
+            int.TryParse(new Regex(colorPattern).Match(@this).Groups["number"].Value, out var count);
+            return count;
+        }
+    }
+}
+
+public record struct BagOfCubesGameState(string TurnsList, BagState Bag) {
+    public string TurnsList { get; init; } = TurnsList.Trim().ToLower();
+    public BagState Bag { get; init; } = Bag;
+}
+
+public record struct BagState(int RedCount, int GreenCount, int BlueCount) {
+    public int RedCount { get; init; } = RedCount;
+    public int GreenCount { get; init; } = GreenCount;
+    public int BlueCount { get; init; } = BlueCount;
+}
