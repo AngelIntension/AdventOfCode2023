9ca9eb51d23aaecd87ea5ce978ab8ea9b27fca63
Byron Burks <byron.burks@gmail.com>
2023-12-23 10:31:33 -0600

    complete day 1

---
+++ AdventTest/TrebuchetTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Trebuchet;
+
+namespace AdventTest; 
+
+public class TrebuchetTest {
+    [Theory]
+    // ReSharper disable StringLiteralTypo
+    [InlineData("1abc2", 12)]
+    [InlineData("pqr3stu8vwx", 38)]
+    [InlineData("a1b2c3d4e5f", 15)]
+    [InlineData("treb7uchet", 77)]
+    // ReSharper restore StringLiteralTypo
+    public static void ToCalibration_GivenNoDigitsAsStrings_ShouldReturnCorrectCalibrationValue(
+        string input, int expectedCalibrationValue) {
+        var calibration = new CalibrationString(input);
+        var expectedCalibration = new Calibration(expectedCalibrationValue);
+        calibration.ToCalibration().Should().Be(expectedCalibration);
+    }
+
+    [Fact]
+    public static void SumTrebuchetCalibrationFile() {
+        var path = Path.GetFullPath(@"D:\Projects\Code\AdventOfCode2023\Trebuchet\TrebuchetCalibration.txt");
+        // ReSharper disable once UnusedVariable
+        var sumCalibrations = File.ReadLines(path)
+                                  .Select(s => new CalibrationString(s))
+                                  .Select(c => c.ToCalibration())
+                                  .Sum(c => c);
+        Assert.True(true);
+    }
+
+    [Theory]
+    // ReSharper disable StringLiteralTypo
+    [InlineData("two1nine", 29)]
+    [InlineData("eightwothree", 83)]
+    [InlineData("abcone2threexyz", 13)]
+    [InlineData("xtwone3four", 24)]
+    [InlineData("4nineeightseven2", 42)]
+    [InlineData("zoneight234", 14)]
+    [InlineData("7pqrstsixteen", 76)]
+    // ReSharper restore StringLiteralTypo
+    public static void ToCalibration_GivenDigitsAsStringsAndNumbers_ShouldReturnCorrectCalibrationValue(
+        string input, int expectedCalibrationValue) {
+        var calibration = new CalibrationString(input);
+        var expectedCalibration = new Calibration(expectedCalibrationValue);
+        calibration.ToCalibration().Should().Be(expectedCalibration);
+    }
+}
---
+++ Trebuchet/Trebuchet.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Trebuchet; 
+
+public static class Trebuchet {
+    public static Calibration ToCalibration(this CalibrationString @this) {
+        var calibrationNumbers = @this.GetCalibrationNumbers();
+        if (calibrationNumbers.Count < 1) {
+            throw new ArgumentException("Calibration string must contain at least one number.");
+        }
+
+        var digits = calibrationNumbers.Select(match => match.ParseDigit()).ToArray();
+        return digits.First() * 10 + digits.Last();
+    }
+
+    private static MatchCollection GetCalibrationNumbers(this CalibrationString @this) {
+        const string pattern = "[123456789]|(?:one|two|three|four|five|six|seven|eight|nine)";
+        return new Regex(pattern).Matches(@this.Data.ToLower());
+    }
+
+    private static int ToDigit(this string @this) {
+        return @this switch {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            _ => throw new ArgumentException("string must contain a valid number to parse")
+        };
+    }
+    
+    private static int ParseDigit(this Capture @this) => int.TryParse(@this.Value, out var digit)
+        ? digit
+        : @this.Value.ToDigit();
+}
+
+public record struct Calibration(int Value) {
+    public int Value { get; init; } = Value;
+    public static implicit operator int(Calibration value) => value.Value;
+    public static implicit operator Calibration(int value) => new(value);
+}
+
+public record struct CalibrationString(string Data) {
+    public string Data { get; init; } = Data;
+    public static implicit operator string(CalibrationString calibrationString) => calibrationString.Data;
+    public static implicit operator CalibrationString(string input) => new(input);
+}
