a520d7a3fdadd42ad9a3770cef8c93781935e283
Byron Burks <AngelIntension@users.noreply.github.com>
2023-12-23 15:44:05 -0600

    complete day 2 puzzles (#1)
    * implement game state validation
    
    * add test to compute sum of valid game numbers
    
    * simplify regex
    
    * functions to compute minimum bag and bag power
    
    * add test to compute sum of minimum bag powers

---
+++ AdventTest/BagOfCubesGameTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+using AutoFixture;
+using BagOfCubesGame;
+using FluentAssertions;
+
+namespace AdventTest; 
+
+public class BagOfCubesGameTest {
+    public BagState TestBag { get; } = new(12, 13, 14);
+
+    [Theory]
+    [InlineData("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", true)]
+    [InlineData("1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", true)]
+    [InlineData("8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", false)]
+    [InlineData("1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", false)]
+    [InlineData("6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", true)]
+    public void IsValid_FunctionTest(string turnsList, bool expected)
+        => new BagOfCubesGameState(turnsList, TestBag)
+          .IsValid()
+          .Should().Be(expected);
+
+    [Fact]
+    public void SumValidGames_GivenGamesToTestFile() {
+        // ReSharper disable once UnusedVariable
+        var sumOfValidGames = 
+            GetGameTuplesFromFile()
+               .Sum(tuple => {
+                    var game = tuple.Item2;
+                    var gameNumber = tuple.Item1;
+                    return game.IsValid() ? gameNumber : 0; });
+        Assert.True(true); }
+
+    [Theory]
+    [InlineData("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48)]
+    [InlineData("1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12)]
+    [InlineData("8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 1560)]
+    [InlineData("1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 630)]
+    [InlineData("6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36)]
+    public void GetMinimumBag_ShouldReturnBagWithExpectedPower(string turnsList, int expectedPowerValue)
+        => new BagOfCubesGameState(turnsList, new Fixture().Create<BagState>())
+          .GetMinimumBag()
+          .GetPower()
+          .Should().Be(new BagPower(expectedPowerValue));
+
+    [Fact]
+    public void SumPowerOfMinimumBags_GivenGamesToTestFile() {
+        // ReSharper disable once UnusedVariable
+        var sumMinimumBagPower = GetGameTuplesFromFile().Sum(tuple => tuple.Item2.GetMinimumBag().GetPower());
+        Assert.True(true); }
+
+    private IEnumerable<Tuple<int, BagOfCubesGameState>> GetGameTuplesFromFile() {
+        var gamesToTest = Path.GetFullPath(@"D:\Projects\Code\AdventOfCode2023\BagOfCubesGame\GamesToTest.txt");
+        var regex = new Regex(@"Game\s+(?'gameNumber'[0123456789]+):(?'turnsList'.*)");
+        return File.ReadLines(gamesToTest)
+                   .Select(line => {
+                        var match = regex.Match(line);
+                        var gameNumber = int.Parse(match.Groups["gameNumber"].Value);
+                        var game = new BagOfCubesGameState(match.Groups["turnsList"].Value, TestBag);
+                        return new Tuple<int, BagOfCubesGameState>(gameNumber, game); }); }
+}
---
+++ BagOfCubesGame/BagOfCubesGame.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+using static System.Math;
+
+namespace BagOfCubesGame; 
+
+public static class BagOfCubesGame {
+    public static bool IsValid(this BagOfCubesGameState @this)
+        => @this.TurnsList
+                .Split(';')
+                .Select(turn => new BagOfCubesGameState(turn, @this.Bag))
+                .Select(game => game.TurnsList.IsValid(game.Bag))
+                .Aggregate((a, b) => a && b);
+
+    public static BagState GetMinimumBag(this BagOfCubesGameState @this)
+        => @this.TurnsList
+                .Split(';')
+                .Aggregate(
+                     new BagState(0, 0, 0),
+                     (accumulator, turn) => new BagState(
+                         Max(turn.GetColorCount("red"), accumulator.RedCount),
+                         Max(turn.GetColorCount("green"), accumulator.GreenCount),
+                         Max(turn.GetColorCount("blue"), accumulator.BlueCount)));
+
+    public static BagPower GetPower(this BagState @this) => new(@this.RedCount * @this.GreenCount * @this.BlueCount);
+
+    private static bool IsValid(this string @this, BagState bag) {
+        if(@this.Contains(';')) {
+            throw new ArgumentException("string must contain a single turn"); }
+
+        var redCount = @this.GetColorCount("red");
+        var greenCount = @this.GetColorCount("green");
+        var blueCount = @this.GetColorCount("blue");
+
+        return redCount <= bag.RedCount
+            && greenCount <= bag.GreenCount
+            && blueCount <= bag.BlueCount; }
+
+    private static int GetColorCount(this string @this, string color) {
+        var regex = new Regex($"(?'number'[0123456789]+)\\s+{color}");
+        int.TryParse(regex.Match(@this).Groups["number"].Value, out var count);
+        return count; }
+}
+
+public record struct BagOfCubesGameState(string TurnsList, BagState Bag) {
+    public string TurnsList { get; init; } = TurnsList.Trim().ToLower();
+    public BagState Bag { get; init; } = Bag; }
+
+public record struct BagState(int RedCount, int GreenCount, int BlueCount) {
+    public int RedCount { get; init; } = RedCount;
+    public int GreenCount { get; init; } = GreenCount;
+    public int BlueCount { get; init; } = BlueCount; }
+
+public record struct BagPower(int Value) {
+    public int Value { get; init; } = Value;
+    public static implicit operator int(BagPower power) => power.Value;
+    public static implicit operator BagPower(int value) => new(value); }
