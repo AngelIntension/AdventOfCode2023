9fc21c3b72565fc07ca1ad9951301b4d29d4f1f2
Byron Burks <byron.burks@gmail.com>
2023-12-24 01:42:13 -0600

    reimplement to eliminate bugs

---
+++ AdventTest/GearRatioTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Immutable;
+using FluentAssertions;
+using GearRatio;
+
+namespace AdventTest {
+    public static class GearRatioTest {
+        private static string TestSchematicContent => "467..114..\n" +
+                                                      "...*......\n" +
+                                                      "..35..633.\n" +
+                                                      "......#...\n" +
+                                                      "617*......\n" +
+                                                      ".....+.58.\n" +
+                                                      "..592.....\n" +
+                                                      "......755.\n" +
+                                                      "...$.*....\n" +
+                                                      ".664.598..\n" +
+                                                      ".......123";
+
+        private static string SchematicPath => @"D:\Projects\Code\AdventOfCode2023\GearRatio\Schematic.txt";
+
+        [Fact]
+        public static void ToParts_ShouldReturnCorrectPartsFromSchematic()
+            => new Schematic(TestSchematicContent)
+              .ToParts()
+              .Should().BeEquivalentTo(
+                   new Part[] {
+                           new(467),
+                           new(35),
+                           new(633),
+                           new(617),
+                           new(592),
+                           new(755),
+                           new(664),
+                           new(598) }
+                      .ToImmutableArray());
+
+        [Fact]
+        public static void ComputeSumOfReferencedPartNumbers() {
+            var parts = new Schematic(File.ReadAllText(Path.GetFullPath(SchematicPath))).ToParts();
+            var sumOfPartNumbers = parts.Sum(part => part.Number);
+            Assert.True(true);
+        }
+    }
+}
--- GearRatio/GearRatio.cs
+++ GearRatio/GearRatio.cs
@@ -5,68 +5,80 @@ namespace GearRatio
 {
     public static class GearRatio {
         public static ImmutableArray<Part> ToParts(this Schematic @this) {
-            var parts = Enumerable.Empty<Part>().ToImmutableArray();
             var grid = @this.Content
                             .Split('\n')
                             .Select(line => line.ToCharArray().ToImmutableArray())
                             .ToImmutableArray();
+
+            var indices = Enumerable.Range(0, grid[0].Length - 1).ToImmutableArray();
+            var symbolCoordinates = Enumerable.Empty<Coordinate>().ToImmutableArray();
             for (var row = 0; row < grid.Length; row++) {
-                var start = 0;
-                while (start < grid[row].Length) {
-                    (start, var end) = grid[row].GetEndpointsOfNextNumber(start);
-                    if (start == -1 || !grid.ContainsAdjacentPartSymbol(row, start, end)) {
-                        break; }
+                symbolCoordinates = symbolCoordinates
+                         .Concat(indices
+                                .Zip(grid[row])
+                                .Where(tuple => tuple.Second.IsPartSymbol())
+                                 // ReSharper disable once AccessToModifiedClosure
+                                .Select(tuple => new Coordinate(row, tuple.First)))
+                         .ToImmutableArray();
+            }
 
-                    var partNumber = int.Parse(grid[row].AsSpan(start, end - start + 1).ToString());
-                    parts = parts.Append(new Part(partNumber)).ToImmutableArray();
-                    start = end + 1; } }
-            return parts; }
+            var partCoordinates = new HashSet<Coordinate>();
+            partCoordinates =
+                symbolCoordinates
+                   .Aggregate(
+                        partCoordinates,
+                        (current, coordinate) => current.Concat(grid.GetAdjacentPartCoordinates(coordinate))
+                                                        .ToHashSet());
 
-        private static (int, int) GetEndpointsOfNextNumber(this ImmutableArray<char> @this, int startIndex) {
-            var start = startIndex;
-            while (start < @this.Length) {
-                if (int.TryParse(@this[start].ToString(), out _)) {
-                    break; }
-                start++; }
-            if (start >= @this.Length) {
-                return (-1, -1); }
-            var end = start + 1;
-            while (end < @this.Length) {
-                if (!int.TryParse(@this[end].ToString(), out _)) {
-                    end--;
-                    break; }
-                end++; }
-            return end >= @this.Length ? (start, end - 1) : (start, end); }
-
-        private static bool ContainsAdjacentPartSymbol(
-            this ImmutableArray<ImmutableArray<char>> @this,
-            int row,
-            int start,
-            int end) {
-            var surroundingCharacters = Enumerable.Empty<char>().ToImmutableArray();
-            var clampedStart = Max(0, start - 1);
-            var clampedEnd = Min(@this[row].Length - 1, end + 1);
-
-            if (row > 0) {
-                surroundingCharacters = @this[row - 1]
-                     .AsSpan(clampedStart, clampedEnd - clampedStart + 1)
-                     .ToImmutableArray(); }
-
-            surroundingCharacters = surroundingCharacters
-                                   .Append(@this[row][clampedStart])
-                                   .Append(@this[row][clampedEnd])
-                                   .ToImmutableArray();
-
-            if (row < @this.Length - 1) {
-                surroundingCharacters = surroundingCharacters
-                                       .Concat(@this[row + 1]
-                                              .AsSpan(clampedStart, clampedEnd - clampedStart + 1)
-                                              .ToArray())
-                                       .ToImmutableArray(); }
-            return surroundingCharacters.Any(c => c.IsPartSymbol()); }
+            return partCoordinates
+                  .Select(coordinate => new Part(grid.GetNumberAt(coordinate)))
+                  .ToImmutableArray();
+        }
 
         private static bool IsPartSymbol(this char @this) => !int.TryParse(@this.ToString(), out _) && @this != '.';
+
+        private static ImmutableArray<Coordinate> GetAdjacentPartCoordinates(
+            this ImmutableArray<ImmutableArray<char>> @this,
+            Coordinate center) {
+            var coordinates = new List<Coordinate>();
+            for (var row = Max(0, center.Row - 1); row <= Min(@this.Length - 1, center.Row + 1); row++) {
+                for (var column = Max(0, center.Column - 1); column <= Min(@this[row].Length -1, center.Column + 1); column++) {
+                    if (@this[row][column].IsDigit()) {
+                        var startOfPartNumber = column;
+                        while (startOfPartNumber > 0) {
+                            if (!@this[row][startOfPartNumber - 1].IsDigit()) {
+                                break; }
+                            startOfPartNumber--;
+                        }
+
+                        var coordinate = new Coordinate(row, startOfPartNumber);
+                        coordinates.Add(coordinate);
+                    }
+                }
+            }
+
+            return coordinates.ToImmutableArray();
+        }
+
+        private static int GetNumberAt(this ImmutableArray<ImmutableArray<char>> @this, Coordinate coordinate) {
+            var row = coordinate.Row;
+            var column = coordinate.Column;
+            var numberString = @this[row]
+                              .Slice(column, @this[row].Length - column)
+                              .TakeWhile(c => c.IsDigit())
+                              .ToArray()
+                              .AsSpan()
+                              .ToString();
+            return int.Parse(numberString);
+        }
+
+        private static bool IsDigit(this char @this) => int.TryParse(@this.ToString(), out _);
+
+        private record struct Coordinate(int Row, int Column) {
+            public int Row { get; init; } = Row;
+            public int Column { get; init; } = Column; }
     }
+
 
     public record struct Part(int Number) {
         public int Number { get; init; } = Number;
