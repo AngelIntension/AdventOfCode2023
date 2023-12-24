2a05d80f8925cbdeebf5b103d84ad8ed7f55854b
Byron Burks <byron.burks@gmail.com>
2023-12-24 16:40:04 -0600

    add functions to get gear locations and gear ratio. compute sum of gear ratios from input schematic

--- AdventTest/GearRatioTest.cs
+++ AdventTest/GearRatioTest.cs
@@ -2,43 +2,57 @@
 using FluentAssertions;
 using GearRatio;
 
-namespace AdventTest {
-    public static class GearRatioTest {
-        private static string TestSchematicContent => "467..114..\n" +
-                                                      "...*......\n" +
-                                                      "..35..633.\n" +
-                                                      "......#...\n" +
-                                                      "617*......\n" +
-                                                      ".....+.58.\n" +
-                                                      "..592.....\n" +
-                                                      "......755.\n" +
-                                                      "...$.*....\n" +
-                                                      ".664.598..\n" +
-                                                      ".......123";
+namespace AdventTest; 
 
-        private static string SchematicPath => @"D:\Projects\Code\AdventOfCode2023\GearRatio\Schematic.txt";
+public static class GearRatioTest {
+    private static string TestSchematicContent => "467..114..\n" +
+                                                  "...*......\n" +
+                                                  "..35..633.\n" +
+                                                  "......#...\n" +
+                                                  "617*......\n" +
+                                                  ".....+.58.\n" +
+                                                  "..592.....\n" +
+                                                  "......755.\n" +
+                                                  "...$.*....\n" +
+                                                  ".664.598..\n" +
+                                                  ".......123";
 
-        [Fact]
-        public static void ToParts_ShouldReturnCorrectPartsFromSchematic()
-            => new Schematic(TestSchematicContent)
-              .ToParts()
-              .Should().BeEquivalentTo(
-                   new Part[] {
-                           new(467),
-                           new(35),
-                           new(633),
-                           new(617),
-                           new(592),
-                           new(755),
-                           new(664),
-                           new(598) }
-                      .ToImmutableArray());
+    private static string SchematicPath => @"D:\Projects\Code\AdventOfCode2023\GearRatio\Schematic.txt";
 
-        [Fact]
-        public static void ComputeSumOfReferencedPartNumbers() {
-            var parts = new Schematic(File.ReadAllText(Path.GetFullPath(SchematicPath))).ToParts();
-            var sumOfPartNumbers = parts.Sum(part => part.Number);
-            Assert.True(true);
-        }
+    [Fact]
+    public static void ToParts_ShouldReturnCorrectPartsFromSchematic()
+        => new Schematic(TestSchematicContent).ToParts().Should().BeEquivalentTo(new Part[] {
+            new(467),
+            new(35),
+            new(633),
+            new(617),
+            new(592),
+            new(755),
+            new(664),
+            new(598)
+        }.ToImmutableArray());
+
+    [Fact]
+    public static void ComputeSumOfReferencedPartNumbers() {
+        var parts = new Schematic(File.ReadAllText(Path.GetFullPath(SchematicPath))).ToParts();
+        // ReSharper disable once UnusedVariable
+        var sumOfPartNumbers = parts.Sum(part => part.Number);
+        Assert.True(true);
+    }
+
+    [Theory]
+    [InlineData(1, 3, 35 * 467)]
+    [InlineData(4, 3, 0)]
+    [InlineData(8, 5, 755 * 598)]
+    public static void GetGearRatio_ShouldReturnExpectedValues(int row, int column, int expectedPower)
+        => new Schematic(TestSchematicContent).GetGearRatio(row, column).Should().Be(expectedPower);
+
+    [Fact]
+    public static void ComputeSumOfGearRatios() {
+        var schematic = new Schematic(File.ReadAllText(Path.GetFullPath(SchematicPath)));
+        // ReSharper disable once UnusedVariable
+        var sumOfGearRatios = schematic.GetGears().Select(tuple => schematic.GetGearRatio(tuple.Row, tuple.Column))
+                                       .Sum();
+        Assert.True(true);
     }
 }
--- GearRatio/GearRatio.cs
+++ GearRatio/GearRatio.cs
@@ -1,90 +1,112 @@
 ﻿using System.Collections.Immutable;
 using static System.Math;
 
-namespace GearRatio
-{
-    public static class GearRatio {
-        public static ImmutableArray<Part> ToParts(this Schematic @this) {
-            var grid = @this.Content
-                            .Split('\n')
-                            .Select(line => line.ToCharArray().ToImmutableArray())
-                            .ToImmutableArray();
+namespace GearRatio;
 
-            var indices = Enumerable.Range(0, grid[0].Length - 1).ToImmutableArray();
-            var symbolCoordinates = Enumerable.Empty<Coordinate>().ToImmutableArray();
-            for (var row = 0; row < grid.Length; row++) {
-                symbolCoordinates = symbolCoordinates
-                         .Concat(indices
-                                .Zip(grid[row])
-                                .Where(tuple => tuple.Second.IsPartSymbol())
-                                 // ReSharper disable once AccessToModifiedClosure
-                                .Select(tuple => new Coordinate(row, tuple.First)))
-                         .ToImmutableArray();
-            }
+public static class GearRatio {
+    public static ImmutableArray<Part> ToParts(this Schematic @this) {
+        var grid = @this.ToGrid();
 
-            var partCoordinates = new HashSet<Coordinate>();
-            partCoordinates =
-                symbolCoordinates
-                   .Aggregate(
-                        partCoordinates,
-                        (current, coordinate) => current.Concat(grid.GetAdjacentPartCoordinates(coordinate))
-                                                        .ToHashSet());
-
-            return partCoordinates
-                  .Select(coordinate => new Part(grid.GetNumberAt(coordinate)))
-                  .ToImmutableArray();
+        var indices = Enumerable.Range(0, grid[0].Length - 1).ToImmutableArray();
+        var symbolCoordinates = Enumerable.Empty<Coordinate>().ToImmutableArray();
+        for (var row = 0; row < grid.Length; row++) {
+            symbolCoordinates = symbolCoordinates.Concat(indices.Zip(grid[row])
+                                                                .Where(tuple => tuple.Second.IsPartSymbol())
+                                                                 // ReSharper disable once AccessToModifiedClosure
+                                                                .Select(tuple => new Coordinate(row, tuple.First)))
+                                                 .ToImmutableArray();
         }
 
-        private static bool IsPartSymbol(this char @this) => !int.TryParse(@this.ToString(), out _) && @this != '.';
+        var partCoordinates = new HashSet<Coordinate>();
+        partCoordinates = symbolCoordinates.Aggregate(partCoordinates,
+                                                      (current, coordinate)
+                                                          => current.Concat(grid.GetAdjacentPartCoordinates(coordinate))
+                                                                    .ToHashSet());
 
-        private static ImmutableArray<Coordinate> GetAdjacentPartCoordinates(
-            this ImmutableArray<ImmutableArray<char>> @this,
-            Coordinate center) {
-            var coordinates = new List<Coordinate>();
-            for (var row = Max(0, center.Row - 1); row <= Min(@this.Length - 1, center.Row + 1); row++) {
-                for (var column = Max(0, center.Column - 1); column <= Min(@this[row].Length -1, center.Column + 1); column++) {
-                    if (@this[row][column].IsDigit()) {
-                        var startOfPartNumber = column;
-                        while (startOfPartNumber > 0) {
-                            if (!@this[row][startOfPartNumber - 1].IsDigit()) {
-                                break; }
-                            startOfPartNumber--;
-                        }
+        return partCoordinates.Select(coordinate => new Part(grid.GetNumberAt(coordinate))).ToImmutableArray();
+    }
 
-                        var coordinate = new Coordinate(row, startOfPartNumber);
-                        coordinates.Add(coordinate);
-                    }
+    public static int GetGearRatio(this Schematic @this, int row, int column) {
+        var grid = @this.ToGrid();
+        if (grid[row][column] != '*') {
+            throw new ArgumentException("there must be a gear at the given coordinate");
+        }
+
+        var adjacentParts = grid.GetAdjacentPartCoordinates(new Coordinate(row, column))
+                                .Select(coordinate => grid.GetNumberAt(coordinate))
+                                .Select(partNumber => new Part(partNumber)).ToImmutableHashSet();
+        if (adjacentParts.Count < 2) {
+            return 0;
+        }
+
+        return adjacentParts.Select(part => part.Number).Aggregate(1, (a, b) => a * b);
+    }
+
+    public static ImmutableHashSet<(int Row, int Column)> GetGears(this Schematic @this) {
+        var grid = @this.ToGrid();
+        var gearCoordinates = Enumerable.Empty<Coordinate>().ToImmutableHashSet();
+        for (var row = 0; row < grid.Length; row++) {
+            for (var column = 0; column < grid[row].Length; column++) {
+                if (grid[row][column] == '*') {
+                    gearCoordinates = gearCoordinates.Add(new Coordinate(row, column));
                 }
             }
-
-            return coordinates.ToImmutableArray();
         }
 
-        private static int GetNumberAt(this ImmutableArray<ImmutableArray<char>> @this, Coordinate coordinate) {
-            var row = coordinate.Row;
-            var column = coordinate.Column;
-            var numberString = @this[row]
-                              .Slice(column, @this[row].Length - column)
-                              .TakeWhile(c => c.IsDigit())
-                              .ToArray()
-                              .AsSpan()
-                              .ToString();
-            return int.Parse(numberString);
+        return gearCoordinates.Select(coordinate => (coordinate.Row, coordinate.Column)).ToImmutableHashSet();
+    }
+
+    private static ImmutableArray<ImmutableArray<char>> ToGrid(this Schematic @this)
+        => @this.Content.Split('\n').Select(line => line.ToCharArray().ToImmutableArray()).ToImmutableArray();
+
+    private static bool IsPartSymbol(this char @this) => !int.TryParse(@this.ToString(), out _) && @this != '.';
+
+    private static ImmutableArray<Coordinate> GetAdjacentPartCoordinates(
+        this ImmutableArray<ImmutableArray<char>> @this, Coordinate center) {
+        var coordinates = new List<Coordinate>();
+        for (var row = Max(0, center.Row - 1); row <= Min(@this.Length - 1, center.Row + 1); row++) {
+            for (var column = Max(0, center.Column - 1);
+                 column <= Min(@this[row].Length - 1, center.Column + 1);
+                 column++) {
+                if (@this[row][column].IsDigit()) {
+                    var startOfPartNumber = column;
+                    while (startOfPartNumber > 0) {
+                        if (!@this[row][startOfPartNumber - 1].IsDigit()) {
+                            break;
+                        }
+
+                        startOfPartNumber--;
+                    }
+
+                    var coordinate = new Coordinate(row, startOfPartNumber);
+                    coordinates.Add(coordinate);
+                }
+            }
         }
 
-        private static bool IsDigit(this char @this) => int.TryParse(@this.ToString(), out _);
-
-        private record struct Coordinate(int Row, int Column) {
-            public int Row { get; init; } = Row;
-            public int Column { get; init; } = Column; }
+        return coordinates.ToImmutableArray();
     }
 
-
-    public record struct Part(int Number) {
-        public int Number { get; init; } = Number;
+    private static int GetNumberAt(this ImmutableArray<ImmutableArray<char>> @this, Coordinate coordinate) {
+        var row = coordinate.Row;
+        var column = coordinate.Column;
+        var numberString = @this[row].Slice(column, @this[row].Length - column).TakeWhile(c => c.IsDigit()).ToArray()
+                                     .AsSpan().ToString();
+        return int.Parse(numberString);
     }
 
-    public record struct Schematic(string Content) {
-        public string Content { get; init; } = Content;
+    private static bool IsDigit(this char @this) => int.TryParse(@this.ToString(), out _);
+
+    private record struct Coordinate(int Row, int Column) {
+        public int Row { get; } = Row;
+        public int Column { get; } = Column;
     }
+}
+
+public record struct Part(int Number) {
+    public int Number { get; init; } = Number;
+}
+
+public record struct Schematic(string Content) {
+    public string Content { get; init; } = Content;
 }
