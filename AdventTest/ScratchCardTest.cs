c4db59372abd8cf023d33a048e870001128d7198
Byron Burks <AngelIntension@users.noreply.github.com>
2023-12-24 20:06:55 -0600

    Main complete day 4 puzzles (#3)
    * add functions to load games from file, parse a game from a single line, and score a game
    
    * compute sum of cards in input file
    
    * complete puzzle 2

---
+++ AdventTest/ScratchCardTest.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Immutable;
+using FluentAssertions;
+using ScratchCards;
+
+namespace AdventTest {
+    public class ScratchCardTest {
+        private static string TestCardsFilepath => @"D:\Projects\Code\AdventOfCode2023\AdventTest\TestScratchCards.txt";
+        private static string InputFilepath => @"D:\Projects\Code\AdventOfCode2023\ScratchCards\Input.txt";
+
+        [Theory]
+        [InlineData("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 4)]
+        [InlineData("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2)]
+        [InlineData("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2)]
+        [InlineData("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1)]
+        [InlineData("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)]
+        [InlineData("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0)]
+        public static void Score_ShouldCorrectlyScoreGivenGame(string input, int expectedScore) {
+            ScratchCardGame.ParseGame(input)
+                           .Game
+                           .Score()
+                           .Should().Be(expectedScore);
+        }
+
+        [Fact]
+        public static void ComputeSumOfGameScoresFromInput() {
+            // ReSharper disable once UnusedVariable
+            var sumOfGameScores = ScratchCardGame
+                                 .GetGamesFromFile(InputFilepath)
+                                 .Select(tuple => tuple.Game.Score())
+                                 .Sum();
+            Assert.True(true);
+        }
+
+        [Fact]
+        public static void ComputeSumUsingCopyRule() {
+            var tuples = ScratchCardGame.GetGamesFromFile(InputFilepath)
+                                        .OrderBy(tuple => tuple.GameNumber)
+                                        .ToImmutableArray();
+            var buckets = Enumerable.Repeat(1, tuples.Length).ToArray();
+            for (var number = 1; number < tuples.Max(t => t.GameNumber); number++) {
+                var countOfCardsWithCurrentNumber = buckets[number - 1];
+                var score = tuples[number - 1].Game.Score();
+                for (var i = number; i < number + score; i++) {
+                    buckets[i] += countOfCardsWithCurrentNumber;
+                }
+            }
+            // ReSharper disable once UnusedVariable
+            var finalCardCount = buckets.Sum();
+            Assert.True(false);
+        }
+    }
+}
---
+++ ScratchCards/ScratchCardGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace ScratchCards
+{
+    public static class ScratchCardGame {
+
+        public static ImmutableArray<(int GameNumber, ScratchCardGameState Game)> GetGamesFromFile(string path)
+            => File.ReadLines(Path.GetFullPath(path))
+                   .Select(ParseGame)
+                   .ToImmutableArray();
+
+        public static (int GameNumber, ScratchCardGameState Game) ParseGame(string line) {
+            const string pattern = @"Card\s+(?'gameNumber'[0123456789]+):(?'winningNumbers'[\s0123456789]+)[|](?'playerNumbers'[\s0123456789]+)";
+            var regex = new Regex(pattern);
+            var match = regex.Match(line);
+            var gameNumber = int.Parse(match.Groups["gameNumber"].Value);
+            var winningNumbers = match.Groups["winningNumbers"].Value.Trim().Split(' ').Where(str => str != string.Empty).Select(int.Parse).ToImmutableHashSet();
+            var playerNumbers = match.Groups["playerNumbers"].Value.Trim().Split(' ').Where(str => str != string.Empty)
+                                     .Select(int.Parse).ToImmutableHashSet();
+            var game = new ScratchCardGameState(winningNumbers, playerNumbers);
+            return (gameNumber, game);
+        }
+
+        public static int Score(this ScratchCardGameState @this)
+            => @this.WinningNumbers.Intersect(@this.PlayerNumbers).Count;
+    }
+
+    public record struct ScratchCardGameState(ImmutableHashSet<int> WinningNumbers, ImmutableHashSet<int> PlayerNumbers) {
+        public ImmutableHashSet<int> WinningNumbers { get; init; } = WinningNumbers;
+        public ImmutableHashSet<int> PlayerNumbers { get; init; } = PlayerNumbers;
+    }
+}
