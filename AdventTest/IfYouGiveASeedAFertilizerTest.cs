a8f723e3b3895cf5e96965a638cb9f9b9082af86
Byron Burks <byron.burks@gmail.com>
2023-12-25 13:31:01 -0600

    add function to generate mapping closures from almanac input text

---
+++ AdventTest/IfYouGiveASeedAFertilizerTest.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using IfYouGiveASeedAFertilizer;
+
+namespace AdventTest {
+    public class IfYouGiveASeedAFertilizerTest {
+        private static string TestAlmanacPath => @"D:\Projects\Code\AdventOfCode2023\AdventTest\TestAlmanac.txt";
+
+        [Theory]
+        [InlineData("seed-to-soil", 98, 50)]
+        [InlineData("seed-to-soil", 99, 51)]
+        [InlineData("seed-to-soil", 50, 52)]
+        [InlineData("seed-to-soil", 51, 53)]
+        [InlineData("seed-to-soil", 1, 1)]
+        [InlineData("seed-to-soil", 10, 10)]
+        [InlineData("soil-to-fertilizer", 15, 0)]
+        [InlineData("soil-to-fertilizer", 52, 37)]
+        [InlineData("soil-to-fertilizer", 0, 39)]
+        public static void CreateTransform_ShouldReturnClosure_WhichCorrectlyComputesTransforms(
+            string mapName, int input, int expected) {
+            var almanacText = File.ReadAllText(Path.GetFullPath(TestAlmanacPath));
+            var almanac = new Almanac(almanacText);
+            var map = almanac.CreateMap(mapName);
+            map(input).Should().Be(expected);
+        }
+    }
+}
---
+++ IfYouGiveASeedAFertilizer/FarmersAlmanac.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+
+namespace IfYouGiveASeedAFertilizer; 
+
+public static class FarmersAlmanac {
+    public static Func<int, int> CreateMap(this Almanac @this, string mapName) {
+        var almanacMaps = @this.AlmanacMaps.Where(map => map.MapName == mapName).ToImmutableArray();
+        return a => Maybe.From(almanacMaps)
+                         .Bind(maps => {
+                              bool IsApplicable(AlmanacMap map) => a >= map.SourceStart && a < map.SourceStart + map.Count;
+                              return maps.Any(IsApplicable)
+                                  ? maps.First(IsApplicable)
+                                  : Maybe<AlmanacMap>.None; })
+                         .Match(
+                              None: () => a,
+                              Some: map => map.DestinationStart + (a - map.SourceStart));
+    }
+}
+
+public record struct Almanac {
+    public ImmutableHashSet<AlmanacMap> AlmanacMaps { get; init; }
+
+    public Almanac(string text) {
+        var lines = text.Split('\n')
+                        .Select(line => line.Trim())
+                        .Where(line => line != string.Empty)
+                        .ToImmutableArray();
+        var mapNames = GetMapNames(lines);
+        AlmanacMaps = mapNames.SelectMany(mapName => GetAlmanacMaps(lines, mapName))
+                              .ToImmutableHashSet();
+    }
+
+    internal static ImmutableHashSet<string> GetMapNames(IEnumerable<string> mapDefinitions) {
+        var mapNameRegex = new Regex(@"(?'mapName'\w+-to-\w+)\s+map:");
+        return mapDefinitions.Select(line => mapNameRegex.Match(line).Groups["mapName"].Value)
+                             .Where(mapName => mapName != string.Empty).ToImmutableHashSet();
+    }
+
+    internal static ImmutableHashSet<AlmanacMap> GetAlmanacMaps(IEnumerable<string> lines, string mapName) {
+        var mapNameRegex = new Regex(@"(?'mapName'\w+-to-\w+)\s+map:");
+        var mapRangeRegex = new Regex(@"(?'destinationStartIndex'\d+)\s+(?'sourceStartIndex'\d+)\s+(?'count'\d+)");
+        return lines
+              .SkipWhile(line => {
+                   if (!mapNameRegex.IsMatch(line)) {
+                       return true;
+                   }
+                   return mapNameRegex.Match(line).Groups["mapName"].Value != mapName; })
+              .Skip(1)
+              .TakeWhile(line => mapRangeRegex.IsMatch(line)).Select(mapRange => {
+                   var match = mapRangeRegex.Match(mapRange);
+                   var sourceStartIndex = int.Parse(match.Groups["sourceStartIndex"].Value);
+                   var destinationStartIndex = int.Parse(match.Groups["destinationStartIndex"].Value);
+                   var count = int.Parse(match.Groups["count"].Value);
+                   return new AlmanacMap(mapName, sourceStartIndex, destinationStartIndex, count); })
+              .ToImmutableHashSet();
+    }
+}
+
+public record struct AlmanacMap(string MapName, int SourceStart, int DestinationStart, int Count);
