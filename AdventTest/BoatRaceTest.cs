f9f43d09f6dfec98ff0ea216661c91aa7c88fe2d
Byron Burks <AngelIntension@users.noreply.github.com>
2023-12-26 13:27:45 -0600

    Complete day 6 puzzles (#5)
    * complete puzzle 1
    
    * complete puzzle 2

---
+++ AdventTest/BoatRaceTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Immutable;
+using FluentAssertions;
+using WaitForIt;
+
+using static WaitForIt.BoatRace;
+// ReSharper disable InconsistentNaming
+
+namespace AdventTest; 
+
+public class BoatRaceTest {
+    [Theory]
+    [InlineData(7, 0, 0)]
+    [InlineData(7, 1, 6)]
+    [InlineData(7, 2, 10)]
+    [InlineData(7, 3, 12)]
+    [InlineData(7, 4, 12)]
+    [InlineData(7, 5, 10)]
+    [InlineData(7, 6, 6)]
+    [InlineData(7, 7, 0)]
+    public static void GetTraveled_mm_ShouldComputeCorrectDistance
+        (int race_ms, int charge_ms, int expected_mm) {
+        var traveled_mm = GetTraveled_mm(race_ms, charge_ms);
+        traveled_mm.Should().Be(expected_mm);
+    }
+
+    [Theory]
+    [InlineData(7, 9, 4)]
+    [InlineData(15, 40, 8)]
+    [InlineData(30, 200, 9)]
+    public static void GetWinningCharge_ms_ShouldReturnCorrectNumberOfChargeSpans
+        (int race_ms, int record_mm, int expectedCount) {
+        var race = new BoatRaceState(race_ms, record_mm);
+        var winningChargeTimes_ms = race.GetWinningCharges_ms();
+        winningChargeTimes_ms.Count.Should().Be(expectedCount);
+    }
+
+    [Fact]
+    public static void ProductOfWinningCharge_msCounts_GivenTestData_ShouldBeExpected() {
+        var races = new BoatRaceState[] {
+            new(7, 9),
+            new(15, 40),
+            new(30, 200)
+        }.ToImmutableArray();
+        var productOfWinningChargeTimeCounts
+            = races.Aggregate(1, (product, race) => product * race.GetWinningCharges_ms().Count);
+        productOfWinningChargeTimeCounts.Should().Be(288);
+    }
+
+    [Fact]
+    public static void ProductOfWinningCharge_msCounts_GivenInputData_ShouldBeExpected() {
+        var races = new BoatRaceState[] {
+            new(61, 430),
+            new(67, 1036),
+            new(75, 1307),
+            new(71, 1150)
+        }.ToImmutableArray();
+        var productOfWinningChargeTimeCounts
+            = races.Aggregate(1, (product, race) => product * race.GetWinningCharges_ms().Count);
+        productOfWinningChargeTimeCounts.Should().Be(316800);
+    }
+    
+    [Fact]
+    public static void ProductOfWinningCharge_msCounts_GivenGoodKerning_ShouldBeExpected() {
+        var races = new BoatRaceState[] { new(71530, 940200) }.ToImmutableArray();
+        var productOfWinningChargeTimeCounts
+            = races.Aggregate(1, (product, race) => product * race.GetWinningCharges_ms().Count);
+        productOfWinningChargeTimeCounts.Should().Be(71503);
+    }
+
+    [Fact]
+    public static void ComputeCountOfWinningChargeTimes_GivenInputWithGoodKerning() {
+        var race = new BoatRaceState(61677571, 430103613071150);
+        var winningChargeTimeCount = race.GetWinningChargeTimeCount();
+        winningChargeTimeCount.Should().Be(45647654);
+    }
+}
---
+++ WaitForIt/BoatRace.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+// ReSharper disable InconsistentNaming
+
+namespace WaitForIt
+{
+    public static class BoatRace
+    {
+        public static long GetTraveled_mm(long race_ms, long charge_ms) {
+            if (race_ms <= 0 || charge_ms <= 0 || charge_ms >= race_ms) {
+                return 0;
+            }
+            const int acceleration_mmPer_ms2 = 1;
+            var speed_mmPer_ms = charge_ms * acceleration_mmPer_ms2;
+            var travel_ms = race_ms - charge_ms;
+            return speed_mmPer_ms * travel_ms;
+        }
+
+        public static ImmutableHashSet<int> GetWinningCharges_ms(this BoatRaceState @this)
+            => Enumerable.Range(1, (int)@this.Race_ms - 1)
+                         .Select(charge_ms => {
+                              var traveled_mm = GetTraveled_mm(@this.Race_ms, charge_ms);
+                              return (traveled_mm, charge_ms); })
+                         .Where(tuple => tuple.traveled_mm > @this.Record_mm)
+                         .Select(tuple => tuple.charge_ms)
+                         .ToImmutableHashSet();
+
+        public static int GetWinningChargeTimeCount(this BoatRaceState @this) {
+            var count = 0;
+            for (var charge_ms = 1; charge_ms < @this.Race_ms; charge_ms++) {
+                if (charge_ms * (@this.Race_ms - charge_ms) > @this.Record_mm) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    public record struct BoatRaceState(long Race_ms, long Record_mm);
+}
