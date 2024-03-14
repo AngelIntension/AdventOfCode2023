acb657e5f76a787a27401041f8dca1517cc9a77e
Byron Burks <byron.burks@gmail.com>
2024-03-14 15:40:11 -0500

    add method to compute hand type

---
+++ AdventTest/CamelCardsTest.cs
@@ -0,0 +1,17 @@
+﻿using CamelCards;
+using FluentAssertions;
+
+namespace AdventTest {
+    public class CamelCardsTest {
+        [Theory]
+        [InlineData("AAAAA", HandType.FiveOfAKind)]
+        [InlineData("AA8AA", HandType.FourOfAKind)]
+        [InlineData("23332", HandType.FullHouse)]
+        [InlineData("TTT98", HandType.ThreeOfAKind)]
+        [InlineData("23432", HandType.TwoPair)]
+        [InlineData("A23A4", HandType.OnePair)]
+        [InlineData("23456", HandType.HighCard)]
+        public void Type_Should_ReturnCorrectHandType(string cards, HandType expected) =>
+            new Hand(cards).Type().Should().Be(expected);
+    }
+}
---
+++ CamelCards/CamelCards.cs
@@ -0,0 +1,38 @@
+﻿namespace CamelCards
+{
+    public record Hand(string Cards);
+
+    public static class CamelCards
+    {
+        public static HandType Type(this Hand @this) =>
+            @this.Split()
+                 .OrderByDescending(group => group.Count)
+                 .ToArray()
+                switch {
+                    [{ Count: 5 }] => HandType.FiveOfAKind,
+                    [{ Count: 4 }, _] => HandType.FourOfAKind,
+                    [{ Count: 3 }, { Count: 2 }] => HandType.FullHouse,
+                    [{ Count: 3 }, { Count: 1 }, { Count: 1 }] => HandType.ThreeOfAKind,
+                    [{ Count: 2 }, { Count: 2 }, _] => HandType.TwoPair,
+                    [{ Count: 2 }, { Count: 1 }, { Count: 1 }, _] => HandType.OnePair,
+                    _ => HandType.HighCard };
+
+        internal static Group[] Split(this Hand @this) => (
+                from rank in @this.Cards.ToCharArray().Distinct()
+                let count = @this.Cards.ToCharArray().Count(card => card == rank)
+                select new Group(rank, count))
+           .ToArray();
+    }
+
+    public enum HandType {
+        FiveOfAKind,
+        FourOfAKind,
+        FullHouse,
+        ThreeOfAKind,
+        TwoPair,
+        OnePair,
+        HighCard
+    }
+
+    internal record Group(char Rank, int Count);
+}
